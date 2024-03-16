0e596af133f7d4c041a1113f1dbe4f06f4e061a9
SAMAKE Issa <samakeissa10@outlook.fr>
2024-03-16 22:21:15 +0100

    ajout du modification

--- Javascript/script.js
+++ Javascript/script.js
@@ -9,26 +9,4 @@ function myFunction() {
     
 } //fin responsive//
 
-// Fonction pour charger et afficher les traductions
-function chargerTraduction(lang) {
-  fetch(lang + 'Data.json')
-    .then(response => response.json())
-    .then(traductions => {
-      Object.keys(traductions).forEach(cle => {
-        const elements = document.querySelectorAll('[data-translate="' + cle + '"]');
-        elements.forEach(element => {
-          element.textContent = traductions[cle];
-        });
-      });
-    })
-    .catch(error => console.error('Erreur lors du chargement des traductions:', error));
-}
-
-// Ajoutez un écouteur d'événements pour chaque drapeau ou option de langue
-document.querySelector('#drapeau-fr').addEventListener('click', () => chargerTraduction('fr'));
-document.querySelector('#drapeau-en').addEventListener('click', () => chargerTraduction('en'));
-// Ajoutez des écouteurs d'événements pour chaque nouveau drapeau
-
-
-
 
